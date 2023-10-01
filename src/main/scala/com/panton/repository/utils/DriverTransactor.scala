ef235c3dfd79159a112b4ae9d219af5e7628b709
panton8 <podvalnikov0@gmail.com>
2023-10-01 16:48:36 +0300

    Add transactor

---
+++ src/main/scala/com/panton/repository/utils/DriverTransactor.scala
@@ -0,0 +1,15 @@
+package com.panton.repository.utils
+
+import cats.effect.IO
+import doobie.util.transactor.Transactor
+
+object DriverTransactor {
+
+  val xa: Transactor[IO] = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",
+    "jdbc:postgresql://localhost:5438/",
+    "postgres",
+    "postgres"
+  )
+}
+
